5ba2d649d2d16d52fc5a57c619ecb618692c5a84
ReaJason <1587047405@qq.com>
2020-11-18 19:01:53 +0800

    update

--- update.py
+++ update.py
@@ -26,5 +26,4 @@ I'm a programming amateur. I'd like to make some interesting and practical tools
 ## 🔰 Statistics 
 
 ![ReaJason's GitHub Stats](https://github-readme-stats.vercel.app/api?username=reajason&show_icons=true&theme=tokyonight)
-![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=reajason&layout=compact&theme=tokyonight)
 ''')
