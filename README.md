8409759fa03d7e41966477fdfe685d4b94ec3811
ReaJason <reajason1225@gmail.com>
2025-04-17 22:11:05 +0800

    fix: sync script text

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 ## 👋 Hi there，I'm <a href="https://reajason.eu.org" target="_blank">ReaJason</a>
 
 - 🔥 I'm a programming amateur.
-- ❄ I'm learning Java Web Cyber Security.
+- ❄ I'm learning Java Web Cybersecurity.
 - ⚡ I'm working on Java(RASP).
-- 📫 contact me by reajason1225@proton.me.
+- 📫 Contact me by reajason1225@proton.me.
 
 ### Latest Blogs
 
--- update.py
+++ update.py
@@ -19,9 +19,9 @@ if __name__ == '__main__':
         f.write(f'''## 👋 Hi there，I'm <a href="https://reajason.eu.org" target="_blank">ReaJason</a>
 
 - 🔥 I'm a programming amateur.
-- ❄ I'm learning Java Web Cyber Security.
+- ❄ I'm learning Java Web Cybersecurity.
 - ⚡ I'm working on Java(RASP).
-- 📫 contact me by reajason1225@proton.me.
+- 📫 Contact me by reajason1225@proton.me.
 
 ### Latest Blogs
 
